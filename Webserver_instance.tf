7a38d6a69b5d0106445f3d360fbfd836db42f275
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-22 18:16:20 +0530

    Update Webserver_instance.tf

--- Webserver_instance.tf
+++ Webserver_instance.tf
@@ -1,1 +1,31 @@
+provider "aws" {
+  region = var.region_webserver
+}
 
+resource "aws_security_group" "webserver_SG" {
+  name = "Webserver_SG"
+  description = "security group for webserver instance"
+  
+#Create rules 
+#for outboud rules 
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_block = ["0.0.0.0/0"]
+  }
+ #for SSH connection 
+    ingress {
+    from_port = 22
+    protocol = "TCP"
+    to_port = 22
+    cidr_block = ["49.207.209.69/32"]
+  }
+ #for outboud rules 
+    eggress {
+    from_port = 0
+    protocol = "TCP"
+    to_port = 0
+    cidr_block = ["0.0.0.0/0"]
+  }
+}

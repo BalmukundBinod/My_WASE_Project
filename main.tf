f7dc724e9e9495e79d084adb809cc3e23cd8a24f
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-22 13:42:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   version = "3.16.0"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "trfm 1st inst" {
+	ami = "ami-062f7200baf2fa504"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "Terraform-Ec2"
+	}
 }

8f0dcb56888f6f6ad655cd589be328e6c22fc31b
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-23 01:43:38 +0530

    Update Webserver_instance.tf

--- Webserver_instance.tf
+++ Webserver_instance.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "hello_terraform" {
-  ami = "ami-062f782c5c05090ae"
+  ami = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
 }

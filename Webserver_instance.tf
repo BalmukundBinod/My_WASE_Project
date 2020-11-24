664f3535b250cfcb6b8ae6381e48d7cf08f17bdb
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-24 15:56:33 +0530

    Update Webserver_instance.tf

--- Webserver_instance.tf
+++ Webserver_instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
 	  region = "${var.region}"
-	}
+}
 resource "aws_instance" "hello_terraform" {
 	  ami = "${var.ami}"
 	  instance_type = "${var.instance_type}"
 	  user_data = "${file("install_httpd.sh")}"
 	  tags = {
 	    Name = "Terraform instance"
-	    }
+	  }
 }

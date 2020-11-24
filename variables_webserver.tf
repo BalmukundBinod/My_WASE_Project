5d9c073ec3afaf3571b6bee860bf6a3d9d82e927
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-24 15:58:16 +0530

    Update variables_webserver.tf

--- variables_webserver.tf
+++ variables_webserver.tf
@@ -1,16 +1,12 @@
-variable "region" 
-	{
-		default = "us-east-2"
-	}
-variable "instance_type" 
-	{
-		default = "t2.micro"
-	}
-variable "ami" 
-	{
-		default = "ami-062f782c5c05090ae"
-	}
-variable "Instance_Name" 
-	{
-		default = "Terraform instance"
-	}
+variable "region" {
+	default = "us-east-2"
+}
+variable "instance_type" {
+	default = "t2.micro"
+}
+variable "ami" {
+	default = "ami-062f782c5c05090ae"
+}
+variable "Instance_Name" {
+	default = "Terraform instance"
+}

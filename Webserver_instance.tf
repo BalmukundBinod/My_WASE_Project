5aab3cdc0e499041f9aab4785cfdc43f781630a9
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-24 08:20:47 +0530

    Update Webserver_instance.tf

--- Webserver_instance.tf
+++ Webserver_instance.tf
@@ -4,12 +4,7 @@ provider "aws" {
 resource "aws_instance" "hello_terraform" {
 	  ami = "ami-062f78fda82ba540f"
 	  instance_type = "t2.micro"
-	  user_data = << EOF 
-		#! /bin/bash
-		yum install httpd -y 
-		service httpd start
-		chkconfig httpd on 	
-	  EOF
+	  user_data = "${file("install_httpd.sh")}"
 	  tags = {
 	    Name = "Terraform instance"
 	    }

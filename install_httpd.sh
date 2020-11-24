d4f0b6ac719841000305aeaee6b2562555090bdb
BalmukundBinod <68558733+BalmukundBinod@users.noreply.github.com>
2020-11-24 08:45:07 +0530

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -2,4 +2,4 @@
 yum install httpd -y 
 service httpd start
 chkconfig httpd on
-echo "<h1> Deployed by terraform</h1>" > /var/www/html/index.html
+echo "<h1> Deployed by terraform</h1>" > /var/wwww/html/index.html
